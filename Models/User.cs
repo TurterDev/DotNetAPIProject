8a3cd4592ba7beacc3fbe2bc3a55c2b305595ec8
TurterDev <turterdev@gmail.com>
2022-11-14 01:49:02 +0700

    first commit

---
+++ Models/User.cs
@@ -0,0 +1,21 @@
+namespace AuthGoogleProject.Models
+{
+    public class User
+    {
+        public int id { get; set; }
+
+        public string firstName { get; set; }
+
+        public string lastname { get; set; }
+
+        public string username { get; set; }
+
+        public string password { get; set; }
+
+        // public string Token { get; set; }
+
+        // public string Role { get; set; }
+
+        public string Email { get; set; }
+    }
+}
